6f537a1b9f31e4ebd762096c2315c854719f3b58
Luqman Fanani <luqman.fanani@gmail.com>
2025-10-24 19:44:05 +0800

    Fix pertemuan 2

---
+++ Meet 2/komen.py
@@ -0,0 +1,14 @@
+"""
+untuk mematikan kode pada awal baris program
+ketik # atau tekan ctrl + /
+
+fungsi dari komen ini seperti notes atau catatan kecil mengingatkan kita
+apa pekerjaan terakhir dan ahrus di lanjutkan dan lain-lain
+"""
+
+nama = "Ikmal" #variabel nama
+# print(nama) #cetak isi dari variabel nama
+
+
+# dibawah ini adalah contoh memodifikiasi isi variabel dengan gabungan teks yang sudah tersedia dalam kode
+print("Salam kenal",nama,"Saya siswa dari SMAN 1 Bone sedang daam pembelajaran Koding")
---
+++ Meet 2/struktur_code.py
@@ -0,0 +1,31 @@
+# ===============================
+# 1. Bagian Komentar & Dokumentasi
+# ===============================
+"""
+Program Perkenalan Siswa/i
+Dibuat oleh: [Nama Siswa]
+Tanggal: 24 Oktober 2025
+"""
+
+
+nama = "Luqman"
+umur = 17
+
+# Menampilkan hasil ke layar
+print("Halo, nama saya", nama)
+print("Saya berumur", umur, "tahun")
+
+"""
+| Bagian             | Fungsi                                     | Contoh                            |
+| ------------------ | ------------------------------------------ | --------------------------------- |
+| **Komentar**       | Memberi keterangan pada kode               | `# Program: Perkenalan Sederhana` |
+| **Variabel**       | Menyimpan data agar bisa digunakan kembali | `nama = "Luqman"`                 |
+| **Output (print)** | Menampilkan teks atau nilai ke layar       | `print("Halo, nama saya", nama)`  |
+
+"""
+
+
+"""
+Komentar → Variabel → Proses → Output
+Ketik → Jalankan → Lihat hasilnya
+"""
